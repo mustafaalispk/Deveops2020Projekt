3f831b46ad2534ea4697be6de96cb98bb57d16bc
mustafaalispk <54665140+mustafaalispk@users.noreply.github.com>
2020-11-18 11:41:03 +0100

    Pagenotfound page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
+import {Routes} from './routes/Routes'
 function App() {
-  return (
-    <div>
-      <h1>
-        Hej devops 2020
-      </h1>      
-    </div>
+  return (   
+      <Routes>
+        <h1>Hej devops 2020</h1>
+      </Routes>     
   );
 }
 
---
+++ src/view/HomeView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HomeView = () => {
+    return (
+        <div>
+            <h1>This is the Homeview</h1>
+        </div>
+    )
+}
